a504d805a5bcdaba31a7fdf7c101b8d08581fd30
Gloriousjoy <gloriousfab99@gmail.com>
2022-08-23 08:48:58 +0300

    commit tree cc5caa65949dc473a7692c07e8579dd25e93b0cc

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const main = document.querySelector('#main');
 main.remove();
 const newHeader = document.createElement("h1");
 newHeader.setAttribute("id", "victory")
-newHeader.innerHTML = '<p>Annastacia is the champion</p>';
+newHeader.innerHTML = '<p>Glorious is the champion</p>';
 Element.append(newHeader);
